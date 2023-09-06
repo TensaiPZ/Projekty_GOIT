60f1012f454595f924d0099b4f399dbb20979dda
TensaiPZ <piotrzywult@gmail.com>
2023-09-06 20:52:36 +0200

    First commit of project six

---
+++ script.js
@@ -0,0 +1,33 @@
+let squares = document.querySelectorAll('.square');
+let colorDisplay = document.querySelector('#color-display');
+let messageDisplay = document.querySelector('#message');
+let h1 = document.querySelector('h1');
+let resetButton = document.querySelector('#reset');
+let modeButton = document.querySelectorAll('.mode');
+let easyButton = document.querySelector('.mode');
+
+let numSquares = 6;
+let colors = [];
+let pickedColor;
+
+init();
+
+// funkcja inicjalizujaca
+function init() {
+    // ustanienie tekstu jaki kolor szukamy
+    setupSquares();
+    // tryb gry
+    // przycisk resetowania
+}
+
+
+function setupSquares(){
+    for (let i = 0; i < squares.length; i++){
+        squares[i].computedStyleMap.backgroundColor = colors[i];
+        squares[i].addEventListener('click', function(){
+            let clickedColor = this.style.backgroundColor;
+            
+            
+        });
+    }
+}
